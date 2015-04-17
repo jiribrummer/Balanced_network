8301a74a2c67a393e20568cd257ce4b34196aba0
jiribrummer <jiribrummer@gmail.com>
2015-04-17 16:26:24 +0200

    Working neuron equation & inhibitory input implemented
    Working neuron equation & inhibitory input implemented.

--- STDP_example.py
+++ STDP_example.py
@@ -9,7 +9,7 @@ vt = -54*mV
 vr = -60*mV
 El = -74*mV
 taue = 5*ms
-F = 10*Hz
+F = 15*Hz
 gmax = .01
 dApre = .01
 dApost = -dApre * taupre / taupost * 1.05
@@ -24,21 +24,25 @@ dge/dt = -ge / taue : 1
 input = PoissonGroup(N, rates=F)
 neurons = NeuronGroup(1, eqs_neurons, threshold='v>vt', reset='v = vr')
 S = Synapses(input, neurons,
-             '''w : 1''',
+             '''w : 1
+                dApre/dt = -Apre / taupre : 1 (event-driven)
+                dApost/dt = -Apost / taupost : 1 (event-driven)''',
+             pre='''ge += w
+                    Apre += dApre
+                    w = clip(w + Apost, 0, gmax)''',
+             post='''Apost += dApost
+                     w = clip(w + Apre, 0, gmax)''',
              connect=True,
              )
 S.w = 'rand() * gmax'
 mon = StateMonitor(S, 'w', record=[0, 1])
 s_mon = SpikeMonitor(input)
 r_mon = PopulationRateMonitor(input)
-M = SpikeMonitor(neurons)
 
-run(1*second, report='text')
-
-figure()
+run(100*second, report='text')
 
 subplot(311)
-plot(S.w / gmax, '.k');
+plot(S.w / gmax, '.k')
 ylabel('Weight / gmax')
 xlabel('Synapse index')
 subplot(312)
@@ -49,12 +53,4 @@ plot(mon.t/second, mon.w.T/gmax)
 xlabel('Time (s)')
 ylabel('Weight / gmax')
 tight_layout()
-
-figure()
-plot(s_mon.t/ms, s_mon.i, '.')
-
-figure()
-subplot(211)
-plot(M.t/ms, M.i, '.')
-
 show()
--- unsupervised_learning.py
+++ unsupervised_learning.py
@@ -10,27 +10,33 @@ print "start unsupervised learning"
 dt = 0.1 * ms		# Timesteps of neurongroup
 Tau_m = 20 * ms     # Membrane time constant
 Vleak = -75 * mV    # Resting membrance potential
-Tdep = 40 * ms      # After depolarisation time constant
+tau_dep = 40 * ms      # After depolarisation time constant
 gleak = 10 * nS     # Leak conductance
 gexc = 1 * nS       # Conductance of excitatory neurons
 ginh = 10 * nS      # Conductance of inhibitory neurons
-Idep = 50 * pA      # After depolarising current
+Idep = 50 * nS      # After depolarising current
 Eexc = 0 * mV       # Reversal potantial excitatory neurons
 Einh = -80 * mV     # Reversal potantial inhbitory neurons
-N = 400             # Number of input neurons (in paper 1000 used)
-F = 10 * Hz         # Frequency of inhibitory input neurons
+N_e = 10            # Number of excitatory input neurons (in paper 1000 used)
+N_i = 10           # Number of inhibitory input neurons (in paper 250 used)
+F_i = 10 * Hz       # Frequency of inhibitory input neurons
+C_m = Tau_m * gleak #
 
 Tau_rp = 5*ms       # Refractory period
 Theta = -50*mV      # Threshold
 Vr = -55*mV         # Reset value after threshold is reached
+tau_exc = 5 * ms
+tau_inh = 10 * ms
 
-Duration = 100*ms
+Duration = 1000*ms
 
 # Integrate and Fire neuron equation
 # Still need to be fixed
 eqs = """
-dv/dt = ( (Vleak-v) ) / (Tau_m ): volt
-# dIdep/dt = -Idep / Tdep : amp
+dv/dt = ( gleak*(Vleak-v) + gexc*(Eexc-v) + ginh*(Einh-v) + Idep) / (C_m): volt
+dgexc/dt = -gexc/tau_exc : siemens
+dginh/dt = -ginh/tau_inh : siemens
+dIdep/dt = -Idep/tau_dep : amp
 """
 
 # Function to determine rates for excitatory input neurons
@@ -50,44 +56,80 @@ def determineRates(N):
 # IF neuron. Equation still to be fixed.
 neuron = NeuronGroup(1, eqs, dt=dt, threshold='v>Theta',
 					reset='v=Vr', refractory=Tau_rp)
+neuron.v = Vleak
 
 # Excitatory artificial input.
-input = PoissonGroup(N, rates=determineRates(N))
+input = PoissonGroup(N_e, rates=determineRates(N_e))
 
-# Synapses. Not in use yet.
-S = Synapses(input, neuron,
-             '''w : 1''',
+# Inhibitory artificial input
+input_inh = PoissonGroup(N_i, rates = F_i)
+
+# Excitatory synapses. 
+S_e = Synapses(input, neuron,
+             '''w : siemens''',
              connect=True,
              )
-# S.w = 'rand() * gmax'
-S.w = 1
+S_e.w = random.gauss(gexc, gexc/3)
+
+# Inhibitory synapses. 
+S_i = Synapses(input_inh, neuron,
+             '''w : siemens''',
+             connect=True,
+             )
+S_i.w = random.gauss(ginh, ginh/3)
 
 # Monitors. Only s_mon is used to visualize input neurons
 # and produces replica of Yger Fig 2A.
-s_mon = SpikeMonitor(input)
-# M= StateMonitor(neuron, 'v', record=True)
-# mon = StateMonitor(S, 'w', record=[0, 1])
-# MS = SpikeMonitor(neuron)
+s_mon_e = SpikeMonitor(input)
+s_mon_i = SpikeMonitor(input_inh)
+M= StateMonitor(neuron, 'v', record=True)
+mon = StateMonitor(S_e, 'w', record=[0, 1])
+MS = SpikeMonitor(neuron)
 
 
 # Main loop to run simulation
 for i in range(10):
-    input.rates = determineRates(N)
-    run(Duration, report='stdout')
+    input.rates = determineRates(N_e)
+    run(Duration/10, report='stdout')
+
+
 
 
 # Plots of which most are not in use, but can be used for
 # neuron and network visualization
 
+def visualise_connectivity(S):
+	Ns = len(S.source)
+	Nt = len(S.target)
+	figure(figsize=(10, 4))
+	subplot(121)
+	plot(zeros(Ns), arange(Ns), 'ok', ms=10)
+	plot(ones(Nt), arange(Nt), 'ok', ms=10)
+	for i, j in zip(S.i, S.j):
+		plot([0, 1], [i, j], '-k')
+	xticks([0, 1], ['Source', 'Target'])
+	ylabel('Neuron index')
+	xlim(-0.1, 1.1)
+	ylim(-1, max(Ns, Nt))
+	subplot(122)
+	plot(S.i, S.j, 'ok')
+	xlim(-1, Ns)
+	ylim(-1, Nt)
+	xlabel('Source neuron index')
+	ylabel('Target neuron index')
+
+
+visualise_connectivity(S_e)
+visualise_connectivity(S_i)
 
 
 # # V-t plots of 4 random excitatory and inhobitory single neurons
-# figure()
-# 
-# title('Excitatory neuron')
-# plot(M[0].t/ms, M[0].v)
-# xlabel('Time (ms)')
-# ylabel('v')
+figure()
+
+title('Excitatory neuron')
+plot(M[0].t/ms, M[0].v)
+xlabel('Time (ms)')
+ylabel('v')
 
 # figure()
 
@@ -106,15 +148,24 @@ for i in range(10):
 
 # plot of excitatory input 
 figure()
-plot(s_mon.t/ms, s_mon.i, '.')
+plot(s_mon_e.t/ms, s_mon_e.i, '.')
 
+# plot of inhibitory input 
+figure()
+plot(s_mon_i.t/ms, s_mon_i.i, '.')
+
+# 
 # figure()
-# subplot(211)
 # plot(MS.t/ms, MS.i, '.')
-#
+
 
 # figure()
 # hist(Nu_i)
+#
+
+# plot for figure 2B
+# figure()
+# scatter()
 
 
 show()
