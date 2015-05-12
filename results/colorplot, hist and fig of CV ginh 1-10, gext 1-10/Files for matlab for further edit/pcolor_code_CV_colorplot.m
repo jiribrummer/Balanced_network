70478bf184d037326de7d9a7dafcaaac95742a2f
jiribrummer <jiribrummer@gmail.com>
2015-05-12 22:41:01 +0200

    Example of 4D plot

--- results/colorplot, hist and fig of CV ginh 1-10, gext 1-10/Files for matlab for further edit/pcolor_code_CV_colorplot.m
+++ results/colorplot, hist and fig of CV ginh 1-10, gext 1-10/Files for matlab for further edit/pcolor_code_CV_colorplot.m
@@ -1,3 +1,11 @@
 %% Create CV colorplot 
 
 pcolor(xvalues(1:9),yvalues(1:9),zvalues)
+
+%% create CV surface plot
+
+surf(xvalues(1:9),yvalues(1:9),zvalues, synchrony)
+xlabel('ginh')
+ylabel('gext')
+zlabel('regularity')
+
