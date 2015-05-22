c9fe2beb5279a2ed7f1c039acf65aef556fec82c
jiribrummer <jiribrummer@gmail.com>
2015-05-22 15:27:13 +0200

    Matlab scripts for analyses after simulations

---
+++ results/color plot/pcolor_code.m
@@ -0,0 +1,12 @@
+%% Create CV colorplot 
+
+
+pcolor_fleur(xvalues,yvalues,zvalues)
+
+%% create CV surface plot
+
+surf(xaxis,yaxis, freq_data, cv_data)
+xlabel('ginh')
+ylabel('gext')
+zlabel('synchrony')
+
--- results/k-means analyses/kmeans_script.m
+++ results/k-means analyses/kmeans_script.m
@@ -1,25 +1,25 @@
 %% K-means clustering for CV values and synchrony measures
 
 
-[rx, cx] = size(xvalues);
-[ry, cy] = size(yvalues);
+[rx, cx] = size(xaxis);
+[ry, cy] = size(yaxis);
 
 
-colorplot_matrix = zeros(cy-1, cx-1);
+colorplot_matrix = zeros(cy, cx);
 
 % Create matrix for k-means clustering
-matrix = cat(2, cv_values', synchrony_values');
+matrix = cat(2, cv_data_kmeans', freq_data_kmeans');
 
 % Returns vector with cluster number as index
 indices = kmeans(matrix, 4);
 
 % Loop to make matrix for colorplot
 index_value = 1;
- for gext_index = 1:cy-1
-    for ginh_index = 1:cx-1
+ for gext_index = 1:cy
+    for ginh_index = 1:cx
         colorplot_matrix(gext_index,ginh_index) = indices(index_value);
         index_value = index_value + 1;
     end
  end
 
-pcolor_fleur(xvalues(1:end-1),yvalues(1:end-1),colorplot_matrix)
+pcolor_fleur(xaxis,yaxis,colorplot_matrix)
