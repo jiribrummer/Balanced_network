44852fb22f69c82aa7eea0e0064a50d1993d95ee
jiribrummer <jiribrummer@gmail.com>
2015-05-26 17:49:05 +0200

    Partial results of large simulation gext 2-10; ginh 1-10; stepsize .5. error occured
    Partial results of large simulation gext 2-10; ginh 1-10; stepsize .5.
    error occured

---
+++ results/Synchrony analysis gext 1.5-10.5, ginh 1-10, stepsize .5/Matlab edit/pcolor_code.m
@@ -0,0 +1,14 @@
+%% Create CV colorplot 
+
+
+
+pcolor_fleur(xaxis,yaxis,zvalues)
+
+
+%% create surface plot
+
+surf(xaxis,yaxis, syn_values, cv_values);
+xlabel('ginh')
+ylabel('gext')
+zlabel('synchrony')
+
